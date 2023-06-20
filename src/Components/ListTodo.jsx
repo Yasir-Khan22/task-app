0d0563bbdf71e87042accfb212f358c212aa4e4d
yasir-khan22 <yasirkhanone22@gmail.com>
2023-06-20 20:44:39 +0500

    bg-gradient change + little modification.

--- src/Components/ListTodo.jsx
+++ src/Components/ListTodo.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { AiFillEdit, AiOutlineCloseCircle } from "react-icons/ai";
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteToDo, editTodo } from '../Reducers/TodoSlicer';
+import { toggleComplete } from '../Reducers/TodoSlicer'; // Added import for toggleComplete
 
 const ListTodo = () => {
 
     const { todoList } = useSelector((state) => state.toDo);
     const dispatch = useDispatch();
+
     const [isEditing, setEditing] = useState(false);
     const [state, setState] = useState({
         id: '',
-        content: '',
+        content: "null",
         contentError: null
     });
 
@@ -36,30 +38,50 @@ const ListTodo = () => {
         setEditing(false);
     }
 
-    return <div>
-        {
-            isEditing ?
-                <div className='form m-[1.3rem] p-[1.3rem] flex justify-between gap-[1rem] text-center text-3xl  bg-gradient-to-r from-custom-black to-custom-red rounded-md'>
+    // added handle toggle for task.
+    const handleToggleComplete = (id, completed) => {
+        dispatch(toggleComplete({ id, completed }));
+    }
+
+    return (
+        <div>
+            {isEditing ? (
+                <div className='form m-[1.3rem] p-[1.3rem] flex justify-between gap-[1rem] text-center text-3xl bg-gradient-to-r from-custom-black to-custom-red rounded-md'>
                     <h2 className='text-[#f3f4f6]'>Update your plan for today</h2>
                     <input type='text' value={content} name='content' onChange={handleChange}></input>
                     <button className="text-[#f3f4f6]" type='button' onClick={edit}>Edit</button>
                     {contentError ? <div className='error'>{contentError}</div> : null}
                 </div>
-                :
+            ) : (
                 <div className='mt-[0.5rem] bg-gray-100 flex flex-col justify-center items-center'>
-                    {
-                        todoList.map(({ id, content }) => {
-                            return <div className='m-[1.3rem] p-[1.3rem] flex justify-between  items-center gap-[1rem] text-center text-3xl text-[#f3f4f6] bg-gradient-to-r from-custom-black to-custom-red rounded-md' key={id}>
-                                <div>{content}</div>
-                                <div className='flex gap-[1rem] text-black text-3xl cursor-pointer'>
-                                    <AiOutlineCloseCircle className="close" onClick={() => dispatch(deleteToDo({ id }))} />
-                                    <AiFillEdit className="edit" onClick={() => onEditToggle(id, content)} />
-                                </div>
+                    {todoList.map(({ id, content, completed }) => (
+                        <div
+                            className={`m-[1.3rem] p-[1.3rem] flex justify-between items-center gap-[1rem] text-center text-3xl ${completed ? 'text-[#f3f4f6] bg-gradient-to-r from-custom-black to-custom-red line-through' : 'text-black'
+                                } rounded-md`}
+                            key={id}
+                        >
+                            <div>{content}</div>
+                            <div className='flex gap-[1rem] text-3xl cursor-pointer'>
+                                <AiOutlineCloseCircle
+                                    className={`close ${completed ? 'completed' : ''}`}
+                                    onClick={() => dispatch(deleteToDo({ id }))}
+                                />
+                                <AiFillEdit
+                                    className={`edit ${completed ? 'completed' : ''}`}
+                                    onClick={() => onEditToggle(id, content)}
+                                />
+                                <input
+                                    type='checkbox'
+                                    checked={completed}
+                                    onChange={() => handleToggleComplete(id, completed)}
+                                />
                             </div>
-                        })
-                    }
-                </div>}
-    </div>;
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default ListTodo;
